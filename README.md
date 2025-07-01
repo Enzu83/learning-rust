820c301123a27384299881a8a9a355dc7c672c76
Enzu83 <enzu.mc@gmail.com>
2025-07-01 20:23:38 +0200

    feat: chapter handling with CLI arg, beginning of chapter 2 and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Leaning Rust
+
+Just following the Rust Book chapters to be more comfortable with Rust.
+
+When I finish it, I'll make a NES emulator in Rust from scratch!
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::io;
+
 fn main() {
-    println!("Hello world");
+    match get_chapter_number() {
+        1 => chapter_1(),
+        2 => chapter_2(),
+        unknown => panic!("Function for chapter {} doesn't exist", unknown),
+    }
+}
+
+fn get_chapter_number() -> u8 {
+    let args: Vec::<String> = env::args().collect();
+    args[1].parse::<u8>().expect("Wrong argument for a chapter number")
+}
+
+fn chapter_1() {
+    println!("Hello, world!");
+}
+
+fn chapter_2() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
